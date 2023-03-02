21cfceda873efa20e7487bdf19ac85450539b156
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-03-02 08:24:55 -0300

    Add files via upload

---
+++ Atividade_JAVA_Conceitos_básicos/E.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class E {
+	static float fatorial(int n) {
+		if (n==0)
+			return 1;
+		return n*fatorial(n-1);
+	}
+	static float calculaE(int n) {
+		if(n==0)
+			return 1;
+		return 1/(fatorial(n))+calculaE(n-1);
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 System.out.print("Digite n: ");
+	     int n = input.nextInt();
+	     System.out.print(calculaE(n));
+	}
+
+}
---
+++ Atividade_JAVA_Conceitos_básicos/fibo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fibo {
+	static long fibo(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		 System.out.print("Digite n: ");
+	     int n = input.nextInt();
+	     System.out.print(fibo(n));
+	}
+
+}
