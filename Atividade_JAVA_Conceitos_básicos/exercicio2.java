7f8c01995bf237a7d6234a96ccd3ee9a860d2726
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-03-02 08:24:07 -0300

    Create exercicio2.java

---
+++ Atividade_JAVA_Conceitos_básicos/exercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; //para fazer o input()
+
+class PauArara{
+	static double calculaPauArara(double h, double min, double s) {
+		return (15.*h)+(0.5*min)+(0.05*s);
+	}
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in); 
+		 System.out.print("Digite as horas: ");
+	     int h = input.nextInt();
+	     System.out.print("Digite os minutos: ");
+	     int min = input.nextInt();
+	     System.out.print("Digite os segundos: ");
+	     int s = input.nextInt();
+	     System.out.print(calculaPauArara(h, min, s));
+	}
+}
