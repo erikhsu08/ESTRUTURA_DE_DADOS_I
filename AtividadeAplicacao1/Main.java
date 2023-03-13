b423a049ede53692836a7a00d7f740b3d8ce301d
MateusKen <54600427+MateusKen@users.noreply.github.com>
2023-03-13 16:17:38 -0300

    Update Main.java
    consegue ler a expressao e dar push na pilha de operacoes se achar as operações

--- AtividadeAplicacao1/Main.java
+++ AtividadeAplicacao1/Main.java
@@ -5,16 +5,20 @@ class Main {
     
     //char[] operacoes = {'+','-','*','/','^'}; 
     //char[] parenteses = {'(', ')'};
-    String operacoes = "+-*/^";
-    String parenteses = "()";
-  
+    String operacoes = "+-*/^(";
+
+    Pilha op = new Pilha(); // pilha para operações
+    
     Scanner s = new Scanner(System.in);
     System.out.print("\nDigite a expressão matemática em notação infixa: ");
     String expressao = s.nextLine();
     
     for (int i = 0; i < expressao.length(); i++){ // for que percorre cada char da expressao 
-      if ((operacoes.indexOf(expressao.charAt(i)) == -1) || (parenteses.indexOf(expressao.charAt(i)) == -1)) // se o expressao[i] não for uma operacao
+
+      if (operacoes.indexOf(expressao.charAt(i)) == -1) // "se o char não estiver em operacoes" 
         System.out.print("\nQual o valor de "+ expressao.charAt(i) + " ?");
+      else // "se o char estiver em operacoes"
+        op.push(expressao.charAt(i));
         
   }
 }}
