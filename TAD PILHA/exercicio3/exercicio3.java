39085676fc7e7c74fe2a3ac7e271658ef1df1709
RodrigoLime <112708096+RodrigoLime@users.noreply.github.com>
2023-03-19 22:24:29 -0300

    Add files via upload

---
+++ TAD PILHA/exercicio3/exercicio3.java
@@ -0,0 +1,53 @@
+package exercicio3;
+
+import java.util.*;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Digite uma expressão: ");
+		String e = s.nextLine();
+		s.close();
+		
+		Pilha p = new Pilha(e.length());
+		boolean verifica = true;
+		int len = e.length();
+		for (int i = 0; i < len; i++) {
+			if (e.charAt(i) == '(' || e.charAt(i) == '[' || e.charAt(i) == '{' || e.charAt(i) == '<' ){
+				p.push(e.charAt(i));	
+			}
+			else if (e.charAt(i) == ')' || e.charAt(i)== ']' || e.charAt(i) == '}' || e.charAt(i) == '>') {
+	             if (p.isEmpty()) {
+	                verifica = false;
+	                break;
+	             }
+	             else {
+	            	 if ((p.top() == '(' && e.charAt(i) != ')') || 
+	            		 (p.top() == '[' && e.charAt(i) != ']') || 
+	            		 (p.top() == '{' && e.charAt(i) != '}') || 
+	            		 (p.top() == '<' && e.charAt(i) != '>')) {
+	            		  p.pop();
+	            		  verifica = false;
+	            		  break;
+	            	 	 }
+	            	 p.pop();
+	             }
+			}
+		}
+		
+		if (!p.isEmpty()) {
+            verifica = false;
+		}
+		if (verifica == true) {
+			System.out.print("A correspondência e ordem dos símbolos da expressão está correta");
+		}
+		else {
+			System.out.print("a correspondência e ordem dos símbolos da expressão não é válida");
+		}
+		
+
+	}
+
+}
