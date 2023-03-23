8eec67185cef21789708db2287c8d5d8d5dada41
Thiago279 <83241823+Thiago279@users.noreply.github.com>
2023-03-23 15:54:15 -0300

    Update Main.java

--- AtividadeAplicacao1/Main.java
+++ AtividadeAplicacao1/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		System.out.print("Digite a expressão matemática em notação infixa: \"");
+		System.out.print("Digite a expressão matemática em notação infixa: ");
 		Scanner s = new Scanner(System.in);
 		String exp = s.nextLine();
 		s.close();
-		if (validaExp(exp)==true)
-			System.out.print("expressao valida ");
-		else
-			System.out.print("expressao invalida ");
+		validaExp(exp);
+			//System.out.print("expressao valida ");
+		//else
+			//System.out.print("expressao invalida ");
 	}
 
 	public static boolean validaExp(String exp) {
@@ -30,11 +30,13 @@ public class Main {
 				contAbre++;
 			if (exp.charAt(i) == ')')
 				contFecha++;
-			if (contFecha > contAbre)
+			if (contFecha > contAbre) {
+				System.out.print("Parenteses incorretos, expressao invalida.");
 				return false;
+			}
 			if(Character.isLetter(exp.charAt(i)))
 					contVariavel++;
-			if (i !=0 && Character.isLetter(exp.charAt(i)) && Character.isLetter(exp.charAt(i-1)) )
+			if (i!= exp.length()-1 && Character.isLetter(exp.charAt(i)) && Character.isLetter(exp.charAt(i+1)) )
 				return false;
 			if(Character.isLetter(exp.charAt(i)) == false && exp.charAt(i) != '(' && exp.charAt(i)!= ')') {
 				int valido = 0;
@@ -42,14 +44,21 @@ public class Main {
 					if (exp.charAt(i) == opValidos[j])
 						valido = 1;
 				}
-			if (valido == 0)
+			if (valido == 0) {
+				System.out.print("Operacao nao reconhecida, expressao invalida.");
 				return false;
 			}
+			}
 		}
-		if (contVariavel == 0)
+		if (contVariavel == 0) {
+			System.out.print("Nenhuma variavel detectada, expressao invalida.");
 			return false;
-		if (contAbre != contFecha)
+		}
+		if (contAbre != contFecha) {
+			System.out.print("Parenteses incorretos, expressao invalida.");
 			return false;
+		}
+		System.out.print("expressao valida ");
 		return true;
 	}
 }
